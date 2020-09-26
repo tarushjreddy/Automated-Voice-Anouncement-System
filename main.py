ca58cd3a77960575c055598d762bba9f8d48c823
tarushjreddy <tarushjreddy63gmail.com>
2020-09-27 00:23:23 +0530

    added complete fntionality

--- main.py
+++ main.py
@@ -24,38 +24,59 @@ def textToSpeech(text, filename):
 
 
 def Evaluvation(filename):
-    # loc = ("announce.xlsx")
     wb = xlrd.open_workbook(filename)
 
     sheet = wb.sheet_by_index(0)
-    # print(sheet.row_values(1, 3))
+
     des = sheet.row_values(1, 4)
     print(des[0])
     tabb = []
     minn = []
     kkk = []
-    details = now.strftime("%d : :%H : %M")
-    for i in range(4):
-        tab = sheet.row_values(i+1, 4)
-        tabone = sheet.row_values(i+1, 5)
-        tabtwo = sheet.row_values(i+1, 7)
-        kkk.append(tabtwo[0])
+    train = []
+    dest = []
+    plat = []
 
-        # print(tab)
+  #  details = now.strftime("%d : :%H : %M")
+
+    for i in range(4):
+        tab = sheet.row_values(i + 1, 4)
+        tabone = sheet.row_values(i + 1, 5)
+        tabtwo = sheet.row_values(i + 1, 7)
+        kkk.append(tabtwo[0])
+        tabthree = sheet.row_values(i + 1, 2)
+
+        train.append(tabthree[0])
+
         minn.append(tabone[0])
         tabb.append(tab[0])
-        # print(int(tabb[1]))
-    # date = int(tabb)
-        # print(tabone[0])
-        textToSpeech("Sangeetha your" +
-                     "Train has arrived", '1omsai.mp3')
-        if int(tabb[i]) == int(now.strftime("%H")) and int(minn[i]) == int(now.strftime("%M")) and int(kkk[i]) == int(now.strftime("%d")):
-            textToSpeech("Sangeetha your" +
-                         "Train has arrived", '1omsai.mp3')
+        dest.append(tabthree[1])
+        plat.append(tabthree[7])
+
+        if int(tabb[i]) == int(now.strftime("%H")) and int(minn[i]) == int(now.strftime("%M")) and int(kkk[i]) == int(
+                now.strftime("%d")):
+
+            textToSpeech(dest[i] + "Train Heading towards" + train[i] + "  " +
+                         " has arrived" + "at Platform number" + str(plat[i]) + "", '1omsai.mp3')
+            textToSpeech("Passengers are requested to board the train from" +
+                         "at Platform number" + str(plat[i]), 'later.mp3')
+
+            alert = AudioSegment.from_mp3("Voice 002-mc..mp3")
+            play(alert)
+            song = AudioSegment.from_mp3("1omsai.mp3")
+
+            play(song)
+            play(alert)
+            songone = AudioSegment.from_mp3("later.mp3")
+            play(songone)
+
+            play(alert)
+
             print(f"{tabb[i]} is omsairam")
+            break
         else:
-            print(
-                f"{kkk[i]} Day {tabb[i]} Hours {minn[i]} Minutes  \tnot defined")
+            print("hello")
+            break
 
     print(minn)
 
